a696424e06a2f502c88bfb58febdec31926feede
sarangkulkarni2209 <148679834+sarangkulkarni2209@users.noreply.github.com>
2023-10-26 13:18:18 +0000

    learn dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello"); // we are using node.js, not browser
+
+console.log(3
+    +
+    3); // code readability should maintain
+
+let name ="Sarang";
+let age = 23;
+let isloggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol =>unique
+
+// object
+
+console.log(typeof "sarang");
+console.log(typeof undefined);
+console.log(typeof null);
